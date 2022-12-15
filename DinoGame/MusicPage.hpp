2b60950d1a73e0cfa19b1af01d039afa93f874d6
sksmaed <107797902+sksmaed@users.noreply.github.com>
2022-12-16 00:05:35 +0800

    音樂頁面

---
+++ DinoGame/MusicPage.hpp
@@ -0,0 +1,73 @@
+#ifndef MusicPage_hpp
+#define MusicPage_hpp
+
+#include <string>
+#include <iostream>
+#include<fstream>
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+const int MUSIC_NUM = 1;
+class MusicPage
+{
+private:
+    // Variables
+    // window
+    sf::Color background_color;
+    sf::RenderWindow* window;
+    sf::VideoMode videoMode;
+    sf::Event ev;
+
+    // Game objects
+    sf::Font font;
+    std::string names[MUSIC_NUM];
+    std::string musicURL[MUSIC_NUM];
+    //std::string coverURL[MUSIC_NUM];
+    //sf::Sprite cover[SKIN_NUM];
+    //sf::Texture cover_texture[SKIN_NUM];
+    sf::Text musicname;
+
+    int index;
+    sf::Texture leftbutton_texture;
+    sf::Sprite leftbutton;
+    sf::IntRect leftbuttonRect;
+    sf::Texture rightbutton_texture;
+    sf::Sprite rightbutton;
+    sf::IntRect rightbuttonRect;
+    sf::Text returnbutton;
+    sf::IntRect returnbuttonRect;
+
+    sf::Music music;
+
+    // Private functions
+    void iniNames(std::string* names, std::string* skinURL);
+    void initVariables();
+    void initWindow();
+    void initObjects();
+
+
+
+public:
+    // Constructors, Destructors
+    MusicPage();
+    ~MusicPage();
+
+    // Accessors
+    sf::Clock time;
+    const bool running() const;
+    const bool ifLeftPressed(sf::Vector2i mousePos) const;
+    const bool ifRightPressed(sf::Vector2i mousePos) const;
+    const bool ifReturnPressed(sf::Vector2i mousePos) const;
+
+    // Functions
+    void pollEvents();
+    void update();
+    void updateMusicname();
+    void updateMusic();
+    //void updateCover();
+    void render();
+};
+
+#endif
